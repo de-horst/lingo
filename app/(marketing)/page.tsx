545bd158dba76f7fee4823532f363773e92da9ca
Denise Horst <denisef.horst@gmail.com>
2024-03-15 21:27:00 -0300

    02 Marketing Skeleton

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg" variant="premium">click me</Button>
+      this is a mkt page
     </div>
   )
 }
+
+
